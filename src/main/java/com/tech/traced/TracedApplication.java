de096f81e8af48bd2f6aeff470a20d9f25cd2976
babafemiolatona <femiolatona@gmail.com>
2026-04-23 18:28:49 +0100

    Initial commit

---
+++ src/main/java/com/tech/traced/TracedApplication.java
@@ -0,0 +1,13 @@
+package com.tech.traced;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TracedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TracedApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tech/traced/TracedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tech.traced;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TracedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
